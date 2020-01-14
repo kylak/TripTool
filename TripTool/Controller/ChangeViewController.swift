b499ddb900dd99851d98daaef328071cd360a644
kylak <gustavberloty2@gmail.com>
2020-01-14 15:28:55 +0100

    Création de la structure MVC et ajout de la tab bar.

--- TripTool/Controller/ChangeViewController.swift
+++ TripTool/Controller/ChangeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  TripTool
 //
 //  Created by Gustav Berloty on 14/01/2020.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class ChangeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ TripTool/Controller/MeteoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MeteoViewController.swift
+//  TripTool
+//
+//  Created by Gustav Berloty on 14/01/2020.
+//  Copyright © 2020 Gustav Berloty. All rights reserved.
+//
+
+import UIKit
+
+class MeteoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
